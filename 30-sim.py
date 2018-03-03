5c1909e5323d20120dabab46cdcefee31bc3acc5
nardtree <gim.kobayashi@gmail.com>
2018-03-04 08:53:58 +0900

    update

--- 30-sim.py
+++ 30-sim.py
@@ -12,6 +12,7 @@ import json
 
 import plyvel
 
+import concurrent.futures
 db = plyvel.DB('feats.ldb', create_if_missing=True) 
 if '--memory' in sys.argv:
   for name in Path('tfidf/').glob('*'):
@@ -21,6 +22,7 @@ if '--memory' in sys.argv:
       print(url)
 
 if '--sim' in sys.argv:
+
   def sim(arg):
     s, t = arg
     score  = 0.
@@ -32,27 +34,36 @@ if '--sim' in sys.argv:
     v2 = math.sqrt( sum([v**2 for v in t.values()]) )
     return score/(v1+v2)
 
+  def _map(arg):
+    url, val = arg
+    source = pickle.loads(val)
+    try:   
+      key_scores = {} 
+      for chunk in Path('./tfidf').glob('*'):
+        for _url, target in pickle.loads( gzip.decompress(chunk.open('rb').read()) ).items():
+          if 'yahoo' in _url:
+            continue
+          # title x title3
+          scores = []
+          for triple in zip(['title', 'keywords', 'description', 'body'], source, target):
+            head = triple[0] 
+            s    = sim(triple[1:])
+            #print(head, s)
+            scores.append( (head, s) )
+          
+          key_scores[ (url, _url) ] = scores
+          #print(sum(map(lambda x:x[1], scores)))
+      ha = hashlib.sha256(bytes(url, 'utf8')).hexdigest()
+      key_scores = [(key,scores) for key, scores in key_scores.items()]
+      key_scores = sorted(key_scores, key=lambda xs:sum(map(lambda x:x[1], xs[1])))[-10:]
+      print(url)
+      print(key_scores)
+      pickle.dump(key_scores, open(f'similarity/{ha}', 'wb'))
+    except Exception as ex:
+      print(f'deep error {ex}')
 
-  onmemory = pickle.load(open('onmemory.pkl','rb'))
-  for url in filter(lambda x:'yahoo' in x, list(onmemory.keys())):
-    print(url)
-    source = onmemory[url]
-      
-    key_scores = {}
-    for _url, target in onmemory.items():
-      if 'yahoo' in _url:
-        continue
-      # title x title3
-      scores = []
-      for triple in zip(['title', 'keywords', 'description', 'body'], source, target):
-        head = triple[0] 
-        s    = sim(triple[1:])
-        #print(head, s)
-        scores.append( (head, s) )
-      
-      key_scores[ (url, _url) ] = scores
-      #print(sum(map(lambda x:x[1], scores)))
-    ha = hashlib.sha256(bytes(url, 'utf8')).hexdigest()
-    key_scores = [(key,scores) for key, scores in key_scores.items()]
-    key_scores = sorted(key_scores, key=lambda xs:sum(map(lambda x:x[1], xs[1])))[-10:]
-    pickle.dump(key_scores, open(f'similarity/{ha}', 'wb'))
+  args = [(url.decode(), val) for url, val in filter(lambda x:'yahoo' in x[0].decode(), db.iterator())]
+  #_map(args[-1])
+  #[_map(arg) for arg in args]
+  with concurrent.futures.ProcessPoolExecutor(max_workers=16) as exe:
+    exe.map(_map, args)
